28281793c97c68fe4a31b0843d3fcbf507a0ded1
Leandro <Leandrozxzgl@gmail.com>
2025-09-20 13:54:51 -0300

    Clase3

---
+++ Clase3/NumerosPares_Impares.py
@@ -0,0 +1,13 @@
+'''
+Numeros Pares e Impares (Tarea)
+'''
+# Solicita al usuario que ingrese un numero
+
+dato=int(input("Ingrese un número: "))
+
+# Verifica si el numero es par o impar
+
+if dato % 2 == 0:
+    print("Numero par")
+else:
+    print("Numero impar")
