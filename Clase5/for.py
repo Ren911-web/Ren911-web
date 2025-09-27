32b0e2b36c9ec0b7e8a6e63c9ac066baaeeea2ca
Leandro <Leandrozxzgl@gmail.com>
2025-09-27 13:05:25 -0300

    Clase5

---
+++ Clase5/for.py
@@ -0,0 +1,6 @@
+'''
+Bucle for
+'''
+
+for i in (6,8,9,4,7,True,"Hola",[0,1,2]):
+    print(f"Datos: {i}")
---
+++ Clase5/funciones.py
@@ -0,0 +1,10 @@
+'''
+Definir funciones
+'''
+
+def sumar(a,b):
+    return a + b
+
+# Ejemplo de uso
+resultado = sumar(3,5)
+print(f"La suma es:{resultado}")
